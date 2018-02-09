534c8d9633966c21c9f44a44327c5748c5a9edf7
Andy Oakley <andy@andyoakley.com>
2018-02-08 22:10:52 -0800

    Add strftime filter

--- mkdocs_jinja2/__init__.py
+++ mkdocs_jinja2/__init__.py
@@ -2,13 +2,23 @@ __version__ = '0.1.0'
 
 from mkdocs.plugins import BasePlugin
 from jinja2 import Environment
+import datetime
+
+def filter_strftime(t, fmt):
+    if type(t) is datetime.datetime:
+        return t.strftime(fmt)
+    else:
+        return datetime.datetime.fromtimestamp(t).strftime(fmt)
 
 class JinjaEnvironment(BasePlugin):
 
     def on_page_content(self, html, page, config, site_navigation):
-        return Environment().from_string(html).render(
+        env = Environment()
+        env.filters['strftime'] = filter_strftime
+
+        return env.from_string(html).render(
                 config=config,
                 site_navigation=site_navigation,
-                pages=site_navigation.pages
+                pages=site_navigation.pages,
                 )
 
